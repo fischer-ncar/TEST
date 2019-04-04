fischer-ncar/TEST

commit fd2fb7ca1c692cec12834c401f49957ca0312af2

Update CIME to ESMCI cime5.8.0 (#2799)
Update CIME to ESMCI cime5.8.0

Squash merge of jgfouca/branch-for-to-acme-2019-03-12

Features:
* Better handling of file permissions when copying files
* Minor JRA & CORE2 DATM forcing changes
* Modifications to allow for E3SM developer tests for the SE SCM
* Report missing variables in cprnc
* Merge to latest pio2 version
* update NYF CORE2 DROF data stream file. (non-BFB)
* Refactor communication between build.py and Makefile: use cmd-line args instead of environment vars.
* Implement share field for e3sm test suites
* Implement support for checksums when download input data

Bug fixes:
* Fixes #2800

[non-BFB]

shr_pio_mod.F90
@@ -178,8 +178,8 @@ contains
        end if
        call pio_set_blocksize(pio_blocksize)
     endif
-
-
+    ! Correct the total_comps value which may be lower in nuopc
+    total_comps = size(comp_iamin)
 
 
     allocate(io_compid(total_comps), io_compname(total_comps))
@@ -229,17 +229,11 @@ contains
              if(ret /= PIO_NOERR) then
                 write(shr_log_unit,*) "ERROR: Setting rearranger options failed"
              end if
-             if(comp_comm_iam(i)==0) then
-                write(shr_log_unit,*) io_compname(i),' : pio_numiotasks = ',pio_comp_settings(i)%pio_numiotasks
-                write(shr_log_unit,*) io_compname(i),' : pio_stride = ',pio_comp_settings(i)%pio_stride
-                write(shr_log_unit,*) io_compname(i),' : pio_root = ',pio_comp_settings(i)%pio_root
-                write(shr_log_unit,*) io_compname(i),' : pio_iotype = ',pio_comp_settings(i)%pio_iotype
-             end if
           end if
        end do
     end if
     do i=1,total_comps
-       if(comp_comm_iam(i)==0) then
+       if(comp_iamin(i)) then
           write(shr_log_unit,*) io_compname(i),' : pio_numiotasks = ',pio_comp_settings(i)%pio_numiotasks
           write(shr_log_unit,*) io_compname(i),' : pio_stride = ',pio_comp_settings(i)%pio_stride
           write(shr_log_unit,*) io_compname(i),' : pio_rearranger = ',pio_comp_settings(i)%pio_rearranger
@@ -247,7 +241,6 @@ contains
           write(shr_log_unit,*) io_compname(i),' : pio_iotype = ',pio_comp_settings(i)%pio_iotype
        end if
     enddo
-
 
   end subroutine shr_pio_init2
 

shr_flux_mod.F90
@@ -298,7 +298,6 @@ SUBROUTINE shr_flux_atmOcn(nMax  ,zbot  ,ubot  ,vbot  ,thbot ,  prec_gust, gust_
    tdiff= tbot - ts
 
    al2 = log(zref/ztref)
-
    DO n=1,nMax
      if (mask(n) /= 0) then
 
@@ -319,18 +318,17 @@ SUBROUTINE shr_flux_atmOcn(nMax  ,zbot  ,ubot  ,vbot  ,thbot ,  prec_gust, gust_
         else
          vmag       = vmag_old
         endif
-         if (use_coldair_outbreak_mod) then
+        if (use_coldair_outbreak_mod) then
             ! Cold Air Outbreak Modification:
             ! Increase windspeed for negative tbot-ts
             ! based on Mahrt & Sun 1995,MWR
 
-            if (tdiff(n).lt.td0) then
-               vscl=min((1._R8+alpha*(abs(tdiff(n)-td0)**0.5_R8/abs(vmag))),maxscl)
-               vmag=vmag*vscl
-               vmag_old=vmag_old*vscl
-            endif
-         endif
-
+           if (tdiff(n).lt.td0) then
+              vscl=min((1._R8+alpha*(abs(tdiff(n)-td0)**0.5_R8/abs(vmag))),maxscl)
+              vmag=vmag*vscl
+              vmag_old=vmag_old*vscl
+           endif
+        endif
         ssq    = 0.98_R8 * qsat(ts(n)) / rbot(n)   ! sea surf hum (kg/kg)
         delt   = thbot(n) - ts(n)                  ! pot temp diff (K)
         delq   = qbot(n) - ssq                     ! spec hum dif (kg/kg)

shr_frz_mod.F90.in
@@ -34,12 +34,13 @@ module shr_frz_mod
 contains
   !===============================================================================
 
-  subroutine shr_frz_freezetemp_init(tfreeze_option)
+  subroutine shr_frz_freezetemp_init(tfreeze_option, mastertask)
 
     implicit none
 
     character(len=*),parameter :: subname = "(shr_frz_freezetemp_init) "
     character(CS),intent(in)  :: tfreeze_option ! option for computing freezing point
+    logical, intent(in)  :: mastertask ! for io
     ! minus1p8 is constant -1.8C
     ! linear_salt is linear equation
     ! mushy for CICE mushy-layer nonlinear equation
@@ -48,13 +49,13 @@ contains
     ! Check tfreeze_option
     !---------------------------------------------------------------
     if (trim(tfreeze_option) == 'minus1p8') then
-       if (shr_log_level>0) write(s_logunit,*) ' tfreeze_option is minus1p8'
+       if (mastertask .and. shr_log_level>0) write(s_logunit,*) ' tfreeze_option is minus1p8'
        tfrz_option = TFREEZE_OPTION_MINUS1P8
     elseif (trim(tfreeze_option) == 'linear_salt') then
-       if (shr_log_level>0) write(s_logunit,*) ' tfreeze_option is linear_salt'
+       if (mastertask .and. shr_log_level>0) write(s_logunit,*) ' tfreeze_option is linear_salt'
        tfrz_option = TFREEZE_OPTION_LINEAR_SALT
     elseif (trim(tfreeze_option) == 'mushy') then
-       if (shr_log_level>0) write(s_logunit,*) ' tfreeze_option is mushy'
+       if (mastertask .and. shr_log_level>0) write(s_logunit,*) ' tfreeze_option is mushy'
        tfrz_option = TFREEZE_OPTION_MUSHY
     else
        call shr_sys_abort(subname//' ERROR: not a valid tfreeze_option '//trim(tfreeze_option))

shr_mem_mod.F90
@@ -19,14 +19,14 @@ MODULE shr_mem_mod
 CONTAINS
   !===============================================================================
 
-  subroutine shr_mem_init(prt)
+  subroutine shr_mem_init(prt, strbuf)
 
     implicit none
 
     !----- arguments -----
 
     logical, optional :: prt
-
+    character(len=*), optional :: strbuf
     !----- local -----
 
     ! --- Memory stats ---
@@ -63,6 +63,12 @@ CONTAINS
        write(s_logunit,'(A,f16.2)') '8 MB memory dealloc in MB is ',(mrss1-mrss2)*mb_blk
        write(s_logunit,'(A,f16.2)') 'Memory block size conversion in bytes is ',mb_blk*1024_shr_kind_r8*1024.0_shr_kind_r8
     endif
+    if (present(strbuf)) then
+       write(strbuf,'(3(A,f16.2))') '8 MB memory   alloc in MB is ',(mrss1-mrss0)*mb_blk, &
+            '\n8 MB memory dealloc in MB is ',(mrss1-mrss2)*mb_blk, &
+            '\nMemory block size conversion in bytes is ',mb_blk*1024_shr_kind_r8*1024.0_shr_kind_r8
+    endif
+
 
   end subroutine shr_mem_init
 

shr_reprosum_mod.F90
@@ -695,8 +695,8 @@ module shr_reprosum_mod
             arr_gmax_exp(:) = -arr_gextremes(1:nflds,1)
             arr_gmin_exp(:) =  arr_gextremes(1:nflds,2)
 
-! if a field is identically zero or contains INFs or NaNs, arr_gmin_exp 
-!   still equals MAXEXPONENT and arr_gmax_exp still equals MINEXPONENT. 
+! if a field is identically zero or contains INFs or NaNs, arr_gmin_exp
+!   still equals MAXEXPONENT and arr_gmax_exp still equals MINEXPONENT.
 !   In this case, set arr_gmin_exp = arr_gmax_exp = MINEXPONENT
             do ifld=1,nflds
                arr_gmin_exp(ifld) = min(arr_gmax_exp(ifld),arr_gmin_exp(ifld))
@@ -892,7 +892,7 @@ module shr_reprosum_mod
                                             ! input array
 
       logical,  intent(in) :: skip_field(nflds)
-         ! flag indicating whether the sum for this field should be 
+         ! flag indicating whether the sum for this field should be
          ! computed or not (used to skip over fields containing
          ! NaN or INF summands)
 

commit 5086e336ec967b3aa3450e92ecc4f483a07ecfb3

Suppress pio logs on non-component-root processes
Fixing a bug that was introduced in PR #2799 (commit :
3eebd7fe90a87f8e824989bddbd63ad053239aad ).

Making sure that we print out PIO logs only from rank 0 of each
component. The old code printed out logs from all ranks in each
component, resulting in large log files.

Fixes #2839

shr_pio_mod.F90
@@ -233,7 +233,7 @@ contains
        end do
     end if
     do i=1,total_comps
-       if(comp_iamin(i)) then
+       if(comp_iamin(i) .and. (comp_comm_iam(i) == 0)) then
           write(shr_log_unit,*) io_compname(i),' : pio_numiotasks = ',pio_comp_settings(i)%pio_numiotasks
           write(shr_log_unit,*) io_compname(i),' : pio_stride = ',pio_comp_settings(i)%pio_stride
           write(shr_log_unit,*) io_compname(i),' : pio_rearranger = ',pio_comp_settings(i)%pio_rearranger